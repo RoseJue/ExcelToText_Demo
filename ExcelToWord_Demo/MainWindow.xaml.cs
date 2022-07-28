a4d80a1992cb166b00b6082d06000d8d23960c2f
RoseJue <2317654783@qq.com>
2022-07-29 00:12:23 +0800

    添加项目文件。

---
+++ ExcelToWord_Demo/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using NPOI.Helper.Excel;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace ExcelToWord_Demo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = ExcelHelper.ExcelConversionDataTable(@"C:\Users\TR\Desktop\Excel\涂布\XM-A1-正极涂布-实操--导出题目.xlsx", "");
+
+            var json = JsonConvert.SerializeObject(dt);
+
+            json = json.Replace("\"", "");
+            json = json.Replace(",", "\n");
+            //json = json.Replace("选项F:null","");
+            var split = json.Split('\n');
+            List<string> list = new List<string>();
+            foreach (var item in split)
+            {
+                if (!item.Contains("null"))
+                {
+                    list.Add(item);
+                }
+            }
+
+            string text = "";
+            foreach (var item in list)
+            {
+                text += item;
+                text += "\n";
+            }
+
+            Textbox1.Text = text;
+
+            //for (int i = 0; i < dt.Rows.Count; i++)
+            //{
+            //    string strName = dt.Rows[i]["题干"].ToString();
+            //}
+        }
+    }
+
+}
